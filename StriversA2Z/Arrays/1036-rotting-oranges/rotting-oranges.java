9535ab4c3af863c6782204345b95a991911efbad
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-05-10 07:45:50 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (38.97%) - LeetSync

---
+++ StriversA2Z/Arrays/1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return -1;
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCount = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0) return 0; 
+
+        int minutes = -1;
+        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            minutes++;
+
+            for (int i = 0; i < size; i++) {
+                int[] pos = queue.poll();
+                int r = pos[0], c = pos[1];
+
+                for (int[] dir : directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        queue.offer(new int[]{nr, nc});
+                        freshCount--;
+                    }
+                }
+            }
+        }
+
+        return freshCount == 0 ? minutes : -1;
+    }
+}
