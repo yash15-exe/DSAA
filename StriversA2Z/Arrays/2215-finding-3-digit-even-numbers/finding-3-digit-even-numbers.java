3bf885b6515dc02387bb925fc5ec671492ec0dce
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-05-12 06:42:28 +0530

    Time: 5 ms (92.10%) | Memory: 44.4 MB (98.26%) - LeetSync

--- StriversA2Z/Arrays/2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ StriversA2Z/Arrays/2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,22 +1,45 @@
 class Solution {
-    public int[] findEvenNumbers(int[] digits) {
-        Set<Integer> result = new HashSet<>();
-        int n = digits.length;
+    // public int[] findEvenNumbers(int[] digits) {
+    //brute force
+    //     Set<Integer> result = new HashSet<>();
+    //     int n = digits.length;
         
-        for (int i = 0; i < n; i++) {
-            if (digits[i] == 0) continue; 
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    if (num % 2 == 0) {
-                        result.add(num);
-                    }
+    //     for (int i = 0; i < n; i++) {
+    //         if (digits[i] == 0) continue; 
+    //         for (int j = 0; j < n; j++) {
+    //             if (i == j) continue;
+    //             for (int k = 0; k < n; k++) {
+    //                 if (k == i || k == j) continue;
+    //                 int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+    //                 if (num % 2 == 0) {
+    //                     result.add(num);
+    //                 }
+    //             }
+    //         }
+    //     }
+        
+    //     return result.stream().sorted().mapToInt(i -> i).toArray();
+    // }
+     public int[] findEvenNumbers(int[] digits) {
+        int[] mpp = new int[10];
+        for (int d : digits) mpp[d]++;
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            if (mpp[i] == 0) continue;
+            mpp[i]--;
+            for (int j = 0; j <= 9; j++) {
+                if (mpp[j] == 0) continue;
+                mpp[j]--;
+                for (int k = 0; k <= 8; k += 2) {
+                    if (mpp[k] == 0) continue;
+                    mpp[k]--;
+                    res.add(i*100 + j*10 + k);
+                    mpp[k]++;
                 }
+                mpp[j]++;
             }
+            mpp[i]++;
         }
-        
-        return result.stream().sorted().mapToInt(i -> i).toArray();
+        return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
