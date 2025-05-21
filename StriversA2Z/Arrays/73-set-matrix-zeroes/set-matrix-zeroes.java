659ce2bd5ee7a0a96cc3d65661ed5e38b5fd0177
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-05-21 09:18:28 +0530

    Time: 1 ms (72.83%) | Memory: 45.6 MB (82.97%) - LeetSync

--- StriversA2Z/Arrays/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ StriversA2Z/Arrays/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,24 +1,82 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set <Integer> rows = new HashSet<>();
-        Set <Integer> columns = new HashSet<>();
+        //better solution....not sooo brute
+        // Set <Integer> rows = new HashSet<>();
+        // Set <Integer> columns = new HashSet<>();
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                if(matrix[i][j] == 0){
-                    rows.add(i);
-                    columns.add(j);
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[i].length; j++){
+        //         if(matrix[i][j] == 0){
+        //             rows.add(i);
+        //             columns.add(j);
+        //         }
+        //     }
+        // }
+        // for(int row: rows){
+        //     for(int col = 0; col < matrix[row].length; col ++){
+        //         matrix[row][col] = 0;
+        //     }
+        // }
+        // for(int column: columns){
+        //     for(int row = 0; row < matrix.length; row ++){
+        //         matrix[row][column] = 0;
+        //     }
+        // }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0; 
                 }
             }
         }
-        for(int row: rows){
-            for(int col = 0; col < matrix[row].length; col ++){
-                matrix[row][col] = 0;
+
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for(int column: columns){
-            for(int row = 0; row < matrix.length; row ++){
-                matrix[row][column] = 0;
+
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRowHasZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstColHasZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
