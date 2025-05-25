4e836623b7c6e9b4cb09e8e4433814d72feba6c8
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-05-25 08:15:15 +0530

    Time: 2 ms (86.77%) | Memory: 48.6 MB (23.79%) - LeetSync

---
+++ StriversA2Z/Arrays/143-reorder-list/reorder-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode node = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = node;
+            node = second;
+            second = temp;
+        }
+
+        ListNode first = head;
+        second = node;
+
+        while (second != null) {
+            ListNode temp1 = first.next, temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }        
+    }
+}
