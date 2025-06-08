57c69dab31d2cfdaae36ce60bf0cfd0c1a98d9ae
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-06-08 06:45:24 +0530

    Time: 4 ms (8.09%) | Memory: 52.8 MB (20.81%) - LeetSync

---
+++ StriversA2Z/Arrays/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int goodNodes(TreeNode root) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(root.val);
+        helper(root, stack);
+        return count;
+        
+    }
+    public void helper(TreeNode root, Stack stack){
+        if(root == null)return;
+        if(root.val >= (int)stack.peek()){
+            stack.push(root.val);
+            count++;
+             helper(root.left, stack);
+        helper(root.right, stack);
+        stack.pop();
+        }
+        else{
+            helper(root.left, stack);
+            helper(root.right, stack);
+        }
+       
+    }
+}
