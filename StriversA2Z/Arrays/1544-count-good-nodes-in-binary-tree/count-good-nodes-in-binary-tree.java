d3816224264ffbd8cf17730f32adfaa5ddeaafd9
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-06-08 06:48:04 +0530

    Time: 2 ms (85.20%) | Memory: 52.8 MB (25.86%) - LeetSync

--- StriversA2Z/Arrays/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ StriversA2Z/Arrays/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,28 +13,47 @@
  *     }
  * }
  */
+ //with stack implementation........
+// class Solution {
+//     int count = 0;
+//     public int goodNodes(TreeNode root) {
+//         Stack<Integer> stack = new Stack<>();
+//         stack.push(root.val);
+//         helper(root, stack);
+//         return count;
+        
+//     }
+//     public void helper(TreeNode root, Stack stack){
+//         if(root == null)return;
+//         if(root.val >= (int)stack.peek()){
+//             stack.push(root.val);
+//             count++;
+//              helper(root.left, stack);
+//         helper(root.right, stack);
+//         stack.pop();
+//         }
+//         else{
+//             helper(root.left, stack);
+//             helper(root.right, stack);
+//         }
+       
+//     }
+// }
+
 class Solution {
     int count = 0;
+
     public int goodNodes(TreeNode root) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(root.val);
-        helper(root, stack);
+        helper(root, root.val);
         return count;
-        
     }
-    public void helper(TreeNode root, Stack stack){
-        if(root == null)return;
-        if(root.val >= (int)stack.peek()){
-            stack.push(root.val);
-            count++;
-             helper(root.left, stack);
-        helper(root.right, stack);
-        stack.pop();
-        }
-        else{
-            helper(root.left, stack);
-            helper(root.right, stack);
-        }
-       
+
+    private void helper(TreeNode node, int maxSoFar) {
+        if (node == null) return;
+
+        if (node.val >= maxSoFar) count++;
+
+        helper(node.left, Math.max(maxSoFar, node.val));
+        helper(node.right, Math.max(maxSoFar, node.val));
     }
 }
