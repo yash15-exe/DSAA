951cabdc4d48450968f9f95ed89364240a330ae2
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-06-14 07:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 72.3 MB (56.79%) - LeetSync

---
+++ StriversA2Z/Arrays/1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+        return (a == c || a == d) ? a : b;
+    }
+}
