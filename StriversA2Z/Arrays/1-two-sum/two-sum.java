ac52a09fd279aad29acacade023e3d4d6ef2aa4f
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-08-14 06:30:37 +0530

    Time: 4 ms (57.91%) | Memory: 45.1 MB (46.94%) - LeetSync

--- StriversA2Z/Arrays/1-two-sum/two-sum.java
+++ StriversA2Z/Arrays/1-two-sum/two-sum.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 class Solution {
-   public static int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for(int i = 0; i<nums.length; i++){
-        map.put(nums[i], i);
-    }
-
-    for(int i = 0; i<nums.length; i++){
-        if(map.containsKey(target-nums[i])&&map.get(target-nums[i])!=i){
-            return new int[]{i, map.get(target-nums[i])};
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+       
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        return new int[]{-1, -1};
     }
-    return new int[]{0, 0};
-    }
-
 }
