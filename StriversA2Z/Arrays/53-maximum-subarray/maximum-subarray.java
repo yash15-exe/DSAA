32f650246568a124514fdbd493d868b5e6fc47ec
Yash Raikar <122626660+yash15-exe@users.noreply.github.com>
2025-08-14 06:49:23 +0530

    Time: 1 ms (99.54%) | Memory: 57 MB (56.13%) - LeetSync

--- StriversA2Z/Arrays/53-maximum-subarray/maximum-subarray.java
+++ StriversA2Z/Arrays/53-maximum-subarray/maximum-subarray.java
@@ -1,13 +1,14 @@
 class Solution {
 
     public int maxSubArray(int[] nums) {
-       int sum = 0, maxSum = Integer.MIN_VALUE;
-
-       for(int i : nums){
-        sum += i;
-        maxSum = Math.max(maxSum, sum);
-        if(sum<0)sum = 0;
-       }
-       return maxSum;
+        int maxSum = Integer.MIN_VALUE;;
+        int sum = 0;
+        for(int i : nums){
+            sum += i;
+             maxSum = Math.max(sum, maxSum);
+            if(sum < 0)sum = 0;
+           
+        }
+        return maxSum;
     }
 }
